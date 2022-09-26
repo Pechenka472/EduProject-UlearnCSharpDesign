9e62debf5f647125abb5c74c79c2a991ca3f82aa
Pechenka472 <a.a.morozov.2002@gmail.com>
2022-09-26 14:43:34 +0500

    hello world

---
+++ hello-world/Program.cs
@@ -0,0 +1,24 @@
+﻿internal static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Start");
+        var a = new A
+        {
+            Number = 1
+        };
+    }
+}
+class A
+{
+    private int _number;
+    public int Number
+    {
+        get => _number;
+        set
+        {
+            Console.WriteLine("Hello, world!");
+            _number = value;
+        }
+    }
+}
